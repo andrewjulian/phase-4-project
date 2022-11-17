08751991cf094085e4654a5c77af1b3282047ff8
+ <+>
2022-11-17 14:46:32 -0700

    double check push

--- client/src/components/OpenQuestions.js
+++ client/src/components/OpenQuestions.js
@@ -14,7 +14,6 @@ const OpenQuestions = ({allQuestions, addQuestion}) => {
     setCreateQuestion(!createQuestion)
   }
 
-
   const onlyOpenQuestions = allQuestions.filter((question) => question.open === true)
 
   const displayOpenQuestions = onlyOpenQuestions.map((question, id) => {
